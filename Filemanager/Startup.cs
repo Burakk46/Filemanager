a1325011afe4f630dd9ae5acf91814612aada1a5
Ardaa <Ardaa@ARDA>
2020-03-06 13:56:48 +0200

    Add project files.

---
+++ Filemanager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Filemanager.Startup))]
+namespace Filemanager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
